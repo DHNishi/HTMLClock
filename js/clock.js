3c69dde9f8a2cc6b2443c1cc7328a3915aebd0fc
Daniel Nishi <sanjiwatsuki@gmail.com>
2015-01-13 08:32:39 -0800

    folders

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+function getTime() {
+    var myDate = new Date();
+    var timeString = myDate.toString();
+    // Ghetto substringing to get the time formatted properly.
+    // I make no guarantees that this stands up to localization.
+    timeString = timeString.substring(timeString.indexOf(":") - 2);
+    timeString = timeString.substring(0, timeString.lastIndexOf(":") + 3);
+    document.getElementById("my_time").innerHTML = timeString;
+    setTimeout(getTime, 1000);
+}
+
+window.onload = getTime;
